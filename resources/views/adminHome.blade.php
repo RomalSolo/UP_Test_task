6106d57801f1b8ecb9748bc55b46311aacf4252f
krome@ua.fm <krome@ua.fm>
2022-07-06 20:59:21 +0300

    fix: upload file blade

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -27,23 +27,5 @@
             </div>
         </div>
     </div>
-    <script>
-        $(function() {
-            $('.toggle-class').change(function() {
-                var status = $(this).prop('checked') == true ? 1 : 0;
-                var id = $(this).data('id');
-
-                $.ajax({
-                    type: "GET",
-                    dataType: "json",
-                    url: '/changeStatus/',
-                    data: {'status': status, 'id': id},
-                    success: function(data){
-                        console.log(data.success)
-                    }
-                });
-            })
-        })
-    </script>
 @endsection
 
